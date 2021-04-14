52d1cba4e1858a5b236847cc3ccf139eb3b56422
Matheus Santos <matheus.ads.study@mail.com>
2021-04-13 21:51:07 -0300

    API Routes no Next.js - Parametrização nas rotas

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    console.log(request.query);
+
+    const users = [
+        { id: 1, nome: 'Matheus' },
+        { id: 2, nome: 'Dani' },
+        { id: 3, nome: 'Diego' },
+    ];
+
+    return response.json(users);
+}
