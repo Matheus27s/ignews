303d1cd9ec111409292d1530a469a21eb0fab932
Matheus Santos <matheus.ads.study@mail.com>
2021-05-03 21:54:35 -0300

    Pagamento no Stripe - Redirecionando para o Stripe

--- src/components/Subscriblebutton/index.tsx
+++ src/components/Subscriblebutton/index.tsx
@@ -1,4 +1,6 @@
 import { signin, useSession } from 'next-auth/client';
+import api from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribleButtonProps {
@@ -9,12 +11,24 @@ export function Subscriblebutton({ priceId }: SubscribleButtonProps) {
 
     const [ session ] = useSession();
 
-    function handleSubscrible() {
+    async function handleSubscrible() {
         if(!session) {
             signin('github');
+            return;
         }
 
-        
+        try {
+            const response = await api.post('/subscrible'); //O arquivo é a própria rota.
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({ sessionId });
+
+        } catch(err) {
+            alert(err.message);
+        }
     }
 
     return  (
--- src/pages/api/subscrible.ts
+++ src/pages/api/subscrible.ts
@@ -1,21 +1,17 @@
 import { getSession } from "next-auth/client";
-import { NextApiRequest, NextApiResponse } from "next-auth/_next";
+import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../services/stripe";
 
 export default async(req: NextApiRequest, res: NextApiResponse) => {
-    //Verifica se o método da requisição é POST
     if(req.method === 'POST') {
-        //Qual o usuário logado na aplicação.
-        const session = await getSession({ req }); // Consegue pegar a sessão por conta do cookie dentro do 'req'
+        const session = await getSession({ req });
 
         const stripeCustomer = await stripe.customers.create({
             email: session.user.email,
         })
 
-        //Criar uma sessão do stripe
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id, //Id do usuário no customer no stripe e nao no banco de dados.
-
+            customer: stripeCustomer.id,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
@@ -29,7 +25,7 @@ export default async(req: NextApiRequest, res: NextApiResponse) => {
 
         return res.status(200).json({ sessionId: stripeCheckoutSession.id })
     } else {
-        res.setHeader('Allow', 'POST'); //Explico pro front end que o método que essa rota aceita é 'POST'
+        res.setHeader('Allow', 'POST'); 
         res.status(405).end('method not allowed');
     }
 }
